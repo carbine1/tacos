a22ed320dd226dba56783d2c77e385e53525733b
carbine1 <carbine1985@163.com>
2022-06-07 18:03:07 +0800

    learning springboot

--- src/test/java/tacos/TacoCloudApplicationTests.java
+++ src/test/java/tacos/TacoCloudApplicationTests.java
@@ -1,10 +1,12 @@
 package tacos;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-@RunWith(SpringRunner.class)
-@SpringBootTset
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
 public class TacoCloudApplicationTests {
     @Test
     public void contextLoads() {
